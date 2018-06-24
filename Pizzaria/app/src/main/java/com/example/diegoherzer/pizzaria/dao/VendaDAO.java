f6c592cd9152469450ac17b575d9f415282c2cec
bruno.allison <bruno.allison@live.com>
2018-06-24 02:48:48 -0400

    atualizações da aplicação

--- Pizzaria/app/src/main/java/com/example/diegoherzer/pizzaria/dao/VendaDAO.java
+++ Pizzaria/app/src/main/java/com/example/diegoherzer/pizzaria/dao/VendaDAO.java
@@ -1,4 +1,111 @@
 package com.example.diegoherzer.pizzaria.dao;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.diegoherzer.pizzaria.model.Produto;
+import com.example.diegoherzer.pizzaria.model.Venda;
+
+import java.util.ArrayList;
+
 public class VendaDAO {
+    private static SQLiteDatabase mDatabase;
+    DBHelper mHelper;
+    private static final String DB_TAG = "database";
+    private Context mContext;
+
+    private static VendaDAO daoInstance;
+
+    public static VendaDAO newInstance(Context context) {
+        if (daoInstance == null) {
+            daoInstance = new VendaDAO(context.getApplicationContext());
+        }
+
+        return daoInstance;
+    }
+
+    private VendaDAO(Context context) {
+        this.mContext = context;
+        mHelper = DBHelper.newInstance(context);
+        mDatabase = mHelper.getWritableDatabase();
+    }
+
+    public static final String[] PROJECTION_VENDA = {
+            DataContract.VendaContract.VENDA_COLUNA_ID,
+            DataContract.VendaContract.VENDA_COLUNA_NOME,
+            DataContract.VendaContract.VENDA_COLUNA_PRECO
+    };
+
+    public int inserir(Venda venda) {
+
+        ContentValues values = new ContentValues();
+        int returnedId = -1;
+
+        values.put(DataContract.ProdutoContract.VENDA_COLUNA_ID, venda.ge());
+        values.put(DataContract.ProdutoContract.VENDA_COLUNA_NOME, venda.getTipo());
+        values.put(DataContract.ProdutoContract.VENDA_COLUNA_PRECO, venda.getDescricao());
+        values.put(DataContract.ProdutoContract.PRODUTO_COLUNA_PRECO, venda.getPreco());
+
+        returnedId = (int) mDatabase.insert(DataContract.ProdutoContract.NOME_TABELA_PRODUTO, null, values);
+        produto.setId(returnedId);
+        return returnedId;
+    }
+
+    public ArrayList<Produto> listarProdutos() {
+
+        ArrayList<Produto> produtos = new ArrayList<>();
+
+        Cursor cursor = mDatabase.query(DataContract.ProdutoContract.NOME_TABELA_PRODUTO, PROJECTION_PRODUTO, null, null, null, null, null);
+
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+                    Produto produto = ProdutoDAO.produtoFromCursor(cursor);
+                    produtos.add(produto);
+                } while (cursor.moveToNext());
+            }
+
+            return produtos;
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+
+    }
+
+    public ArrayList<String> listarNomesProdutos() {
+
+        ArrayList<String> produtos = new ArrayList<>();
+
+        Cursor cursor = mDatabase.query(DataContract.ProdutoContract.NOME_TABELA_PRODUTO, PROJECTION_PRODUTO, null, null, null, null, null);
+
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+                    Produto produto = ProdutoDAO.produtoFromCursor(cursor);
+                    produtos.add(produto.getNome());
+                } while (cursor.moveToNext());
+            }
+
+            return produtos;
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+
+    }
+
+    private static Produto produtoFromCursor(Cursor cursor) {
+        int id = cursor.getInt(cursor.getColumnIndex(DataContract.ProdutoContract.PRODUTO_COLUNA_ID));
+        String nome = cursor.getString(cursor.getColumnIndex(DataContract.ProdutoContract.PRODUTO_COLUNA_NOME));
+        String tipo = cursor.getString(cursor.getColumnIndex(DataContract.ProdutoContract.PRODUTO_COLUNA_TIPO));
+        String descricao = cursor.getString(cursor.getColumnIndex(DataContract.ProdutoContract.PRODUTO_COLUNA_DESCRICAO));
+        Double preco = cursor.getDouble(cursor.getColumnIndex(DataContract.ProdutoContract.PRODUTO_COLUNA_PRECO));
+
+        return new Produto(id, nome, tipo, descricao, preco);
+    }
 }
--- Pizzaria/app/src/main/java/com/example/diegoherzer/pizzaria/model/Venda.java
+++ Pizzaria/app/src/main/java/com/example/diegoherzer/pizzaria/model/Venda.java
@@ -3,6 +3,7 @@ package com.example.diegoherzer.pizzaria.model;
 import java.util.ArrayList;
 
 public class Venda {
+    private int id;
     private ArrayList<ItemVenda> itens;
 
     public Venda() {
@@ -11,6 +12,19 @@ public class Venda {
 
     public Venda(ArrayList<ItemVenda> itens) {
         this.itens = itens;
+    }
+
+    public Venda(int id, ArrayList<ItemVenda> itens) {
+        this.id = id;
+        this.itens = itens;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public ArrayList<ItemVenda> getItens() {
